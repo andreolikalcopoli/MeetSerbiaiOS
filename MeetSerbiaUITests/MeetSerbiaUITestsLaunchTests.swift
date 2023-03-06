1fce25a2ac06269a3673b2384328a838f98251d9
NemanjaDucic <ducicnemanja@gmail.com>
2023-03-06 08:07:03 +0100

    Initial Commit

---
+++ MeetSerbiaUITests/MeetSerbiaUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MeetSerbiaUITestsLaunchTests.swift
+//  MeetSerbiaUITests
+//
+//  Created by Nemanja Ducic on 6.3.23..
+//
+
+import XCTest
+
+final class MeetSerbiaUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
