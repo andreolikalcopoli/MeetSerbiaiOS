3df0ed555604f0ed6cc41121f603f85f5fd73346
NemanjaDucic <ducicnemanja@gmail.com>
2023-03-10 08:51:53 +0100

    initial

---
+++ Pods/Target Support Files/Pods-MeetSerbia-MeetSerbiaUITests/Pods-MeetSerbia-MeetSerbiaUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MeetSerbia_MeetSerbiaUITests : NSObject
+@end
+@implementation PodsDummy_Pods_MeetSerbia_MeetSerbiaUITests
+@end
---
+++ Pods/Target Support Files/Pods-MeetSerbia/Pods-MeetSerbia-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MeetSerbia : NSObject
+@end
+@implementation PodsDummy_Pods_MeetSerbia
+@end
